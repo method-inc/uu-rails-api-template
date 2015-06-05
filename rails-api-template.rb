6b9391edd23187ca716814e92a05689dfc9bd91c
Glenn Goodrich <glenn.goodrich@gmail.com>
2015-06-05 11:24:08 -0400

    Let's just do it in source_paths

--- rails-api-template.rb
+++ rails-api-template.rb
@@ -1,29 +1,25 @@
 def source_paths
   Array(super) +
-    [File.expand_path(File.dirname(__FILE__))]
-end
+    if __FILE__ =~ %r{\Ahttps?://}
+      source_paths.unshift(tempdir = Dir.mktmpdir("uu-rails-api-template-"))
+      at_exit { FileUtils.remove_entry(tempdir) }
+      git :clone => [
+        "--quiet",
+        "https://github.com/skookum/uu-rails-api-template.git",
+        tempdir
+      ].map(&:shellescape).join(" ")
 
-require "shellwords"
+      [tempdir]
+    else
+      [File.expand_path(File.dirname(__FILE__))]
+    end
+end
 #
 # Add this template directory to source_paths so that Thor actions like
 # copy_file and template resolve against our source files. If this file was
 # invoked remotely via HTTP, that means the files are not present locally.
 # In that case, use `git clone` to download them to a local temporary dir.
-def add_template_repository_to_source_path
-  if __FILE__ =~ %r{\Ahttps?://}
-    source_paths.unshift(tempdir = Dir.mktmpdir("uu-rails-api-template-"))
-    at_exit { FileUtils.remove_entry(tempdir) }
-    git :clone => [
-      "--quiet",
-      "https://github.com/skookum/uu-rails-api-template.git",
-      tempdir
-    ].map(&:shellescape).join(" ")
-  else
-    source_paths.unshift(File.dirname(__FILE__))
-  end
-end
 
-add_template_repository_to_source_path
 
 remove_file "Gemfile"
 run "touch Gemfile"
@@ -90,7 +86,7 @@ production:
   <<: *default
   database: #{app_name}_production
 
-EOF
+  EOF
   end
 end
 
